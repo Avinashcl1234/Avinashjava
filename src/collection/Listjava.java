ac70a7bb00c6f9cfcdc673ab62b4fb1823b55fd5
vis123 <avinashsh97@gmail.com>
2022-07-23 17:59:06 +0530

    newcode

---
+++ src/collection/Listjava.java
@@ -0,0 +1,31 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Listjava {
+public static void main(String[] args) {
+	List<String> li=new ArrayList<String>();
+    li.add("apple");
+    li.add("orange");
+    li.add("banana");
+    li.add("tomato");
+    li.add("fruite");
+for (int i = 5; i == li.size(); i++) {
+//	System.out.println(li);
+	
+	
+}    
+  
+List<Integer> in= new ArrayList<Integer>();
+in.add(6);
+in.add(5);
+in.add(4);
+in.add(3);
+in.add(2);
+in.add(6);
+System.out.println(in);
+
+}
+}
